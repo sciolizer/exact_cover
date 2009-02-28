626df4589d928271264bb2c1036f6d8837ec2ea0
Joshua Ball <JoshBBall@gmail.com>
2009-02-28 14:10:13 -0800

    Removed files that will not be a presented to bahaskell.

--- Inst/AbstractExactCover.hs
+++
@@ -1,5 +0,0 @@
-module Inst.AbstractExactCover where
-
--- given two sets P and Q and a binary relation between them,
--- find a subset P* of P such that each element of Q is related
--- to exactly one element of P*
